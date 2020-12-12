3877751993fa4156b7a5294ceec7348734088c07
Arthur Harduim <arthur.harduim@rioenergy.com.br>
2020-12-12 11:50:53 -0300

    Switching config format to json.

--- etl.py
+++ etl.py
@@ -1,32 +1,21 @@
-import configparser
-import psycopg2
 from sql_queries import copy_table_queries, insert_table_queries
+from sqlalchemy import create_engine
+from json import loads
 
 
 def load_staging_tables(cur, conn):
     for query in copy_table_queries:
         cur.execute(query)
-        conn.commit()
 
 
 def insert_tables(cur, conn):
     for query in insert_table_queries:
         cur.execute(query)
-        conn.commit()
-
-
-def main():
-    config = configparser.ConfigParser()
-    config.read('dwh.cfg')
-
-    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
-    cur = conn.cursor()
-    
-    load_staging_tables(cur, conn)
-    insert_tables(cur, conn)
-
-    conn.close()
 
 
 if __name__ == "__main__":
-    main()
+    with open("dwh.json") as cf:
+        config = loads(cf.read())
+
+    uri = "redshift+psycopg2://{user}:{password}@{host}:{port}/{db}".format(**config["redshift"])
+    conn = create_engine(uri)
