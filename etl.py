77a759d2bc765b6a9d6d5bbcc4dda1e19034d3fd
Arthur Harduim <arthur.harduim@rioenergy.com.br>
2020-12-12 12:26:53 -0300

    moved connection to sql_queries.py

--- etl.py
+++ etl.py
@@ -1,14 +1,13 @@
-from sql_queries import copy_table_queries, insert_table_queries
-from sqlalchemy import create_engine
+from sql_queries import copy_table_queries, insert_table_queries, get_connection
 from json import loads
 
 
-def load_staging_tables(cur, conn):
+def load_staging_tables(cur):
     for query in copy_table_queries:
         cur.execute(query)
 
 
-def insert_tables(cur, conn):
+def insert_tables(cur):
     for query in insert_table_queries:
         cur.execute(query)
 
@@ -17,5 +16,4 @@ if __name__ == "__main__":
     with open("dwh.json") as cf:
         config = loads(cf.read())
 
-    uri = "redshift+psycopg2://{user}:{password}@{host}:{port}/{db}".format(**config["redshift"])
-    conn = create_engine(uri)
+    conn = get_connection(config["redshift"])
