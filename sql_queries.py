9235cd9080870fbfee27af60e1d9b4980ade40b5
Arthur Harduim <arthur.harduim@rioenergy.com.br>
2020-12-18 19:24:36 -0300

    fixing some datatype and querie issues

--- sql_queries.py
+++ sql_queries.py
@@ -70,79 +70,81 @@ time_table_drop = "DROP TABLE IF EXISTS time;"
 # CREATE TABLE STATEMENTS
 staging_events_table_create = """
 CREATE TABLE staging_events (
-    "artist" varchar NULL,
-    "auth" varchar NULL,
-    "firstName" varchar NULL,
-    "gender" varchar NULL,
-    "itemInSession" int NULL,
-    "lastName" varchar NULL,
-    "lengh" varchar NULL,
-    "level" varchar NULL,
-    "location" varchar NULL,
-    "method" varchar NULL,
-    "page" varchar NULL,
-    "registration" float NULL,
-    "sessionId" varchar NULL,
-    "song" varchar NULL,
-    "status" int NULL,
-    "ts" bigint NULL,
-    "userAgent" varchar NULL,
-    "userId" varchar NULL
+    "artist" varchar,
+    "auth" varchar,
+    "firstName" varchar,
+    "gender" varchar,
+    "itemInSession" int,
+    "lastName" varchar,
+    "lengh" float,
+    "level" varchar,
+    "location" varchar,
+    "method" varchar,
+    "page" varchar,
+    "registration" float,
+    "sessionId" int,
+    "song" varchar,
+    "status" int,
+    "ts" bigint,
+    "userAgent" varchar,
+    "userId" int
 );
 """
 
 staging_songs_table_create = """
 CREATE TABLE staging_songs (
-    "num_songs" int NULL,
-    "artist_id" varchar NULL,
-    "artist_latitude" varchar NULL,
-    "artist_longitude" varchar NULL,
-    "artist_location" varchar NULL,
-    "artist_name" varchar NULL,
-    "song_id" varchar NULL,
-    "title" varchar NULL,
-    "year" int NULL,
-    "duration" float NULL
+    "num_songs" int,
+    "artist_id" varchar,
+    "artist_latitude" float,
+    "artist_longitude" float,
+    "artist_location" varchar,
+    "artist_name" varchar,
+    "song_id" varchar,
+    "title" varchar,
+    "year" int,
+    "duration" float
 );
 """
 
 songplay_table_create = """
 CREATE TABLE songplays (
-    "songplay_id" bigint IDENTITY(0,1) NOT NULL,
-    "start_time" timestamp NULL,
-    "user_id" int NULL,
+    "songplay_id" bigint IDENTITY(0,1),
+    "start_time" timestamp NOT NULL sortkey distkey,
+    "user_id" int NOT NULL,
     "level" varchar NULL,
-    "song_id" varchar NULL,
-    "artist_id" varchar NULL,
+    "song_id" varchar NOT NULL,
+    "artist_id" varchar NOT NULL,
     "session_id" int NULL,
     "location" text NULL,
     "user_agent" text NULL,
     CONSTRAINT songplays_pkey PRIMARY KEY (songplay_id),
     CONSTRAINT songplays_un UNIQUE (start_time, user_id, song_id),
-    CONSTRAINT songplays_fk_2 FOREIGN KEY (user_id) REFERENCES users(user_id)
+    CONSTRAINT songplays_fk_2 FOREIGN KEY (user_id) REFERENCES users(user_id),
+    CONSTRAINT songplays_fk_3 FOREIGN KEY (song_id) REFERENCES songs(song_id),
+    CONSTRAINT songplays_fk_4 FOREIGN KEY (artist_id) REFERENCES artists(artist_id)
 );
 """
 
 user_table_create = """
 CREATE TABLE users (
-    "user_id" int NOT NULL,
+    "user_id" int,
     "first_name" varchar NULL,
     "last_name" varchar NULL,
     "gender" char NULL,
     "level" varchar NULL,
     CONSTRAINT users_pk PRIMARY KEY (user_id)
-);
+) diststyle all;
 """
 
 song_table_create = """
 CREATE TABLE songs (
-    "song_id" varchar NOT NULL,
+    "song_id" varchar,
     "title" varchar NOT NULL,
     "artist_id" varchar NOT NULL,
     "year" int NOT NULL,
     "duration" float NOT NULL,
     CONSTRAINT songs_pk PRIMARY KEY (song_id)
-);
+) diststyle all;
 """
 
 artist_table_create = """
@@ -158,7 +160,7 @@ CREATE TABLE artists (
 
 time_table_create = """
 CREATE TABLE "time" (
-    "start_time" timestamp NOT NULL,
+    "start_time" timestamp sortkey distkey,
     "hour" int NOT NULL,
     "day" int NOT NULL,
     "week" int NOT NULL,
@@ -188,54 +190,41 @@ REGION 'us-west-2';
 songplay_table_insert = """
 INSERT INTO songplays
 (start_time, user_id, "level", song_id, artist_id, session_id, location, user_agent)
-SELECT TIMESTAMP 'epoch' + "ts"/1000 * INTERVAL '1 second',
-        CAST("userid" as int) as user_id,
+SELECT TIMESTAMP 'epoch' + ts/1000 * INTERVAL '1 second',
+        user_id,
         "level",
-        (
-            SELECT song_id
-            FROM songs
-            INNER JOIN artists on songs.artist_id = artists.artist_id
-            WHERE title = song and artist = artists."name"
-        ),
-        (SELECT artist_id FROM artists WHERE artist = artists."name"),
-        CAST(sessionid AS INT),
+        songs.song_id,
+        artists.artist_id,
+        sessionid,
         location,
         useragent
-FROM staging_events as se
-WHERE auth != 'Logged Out'
+FROM staging_events
+INNER JOIN artists on artists.name = staging_events.artist
+INNER JOIN songs on songs.title = staging_events.song
+WHERE page='NextSong'
 """
 
 user_table_insert = """
 INSERT INTO users
-SELECT cast("userId" as integer),
-       max("firstName"),
-       max("lastName"),
-       max("gender"),
-       max("level")
+SELECT DISTINCT userId, firstName, lastName, gender, "level"
 FROM staging_events
-WHERE auth != 'Logged Out'
-GROUP BY userid;
+WHERE page='NextSong'
 """
 
 song_table_insert = """
 INSERT INTO songs
-SELECT "song_id",
-        "title",
-        "artist_id",
-        "year",
-        "duration"
+SELECT song_id, title, artist_id, year, duration
 FROM staging_songs;
 """
 
 artist_table_insert = """
 INSERT INTO artists
-SELECT "artist_id",
-        MAX("artist_name"),
-        MAX("artist_location"),
-        CAST(MAX("artist_latitude") as float),
-        CAST(MAX("artist_longitude") as float)
+SELECT DISTINCT artist_id,
+        artist_name,
+        artist_location,
+        artist_latitude,
+        artist_longitude
 FROM staging_songs
-GROUP BY "artist_id"
 """
 
 time_table_insert = """
